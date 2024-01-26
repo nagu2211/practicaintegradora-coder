d2d985c9925c86ede5f84ccecfd2926ef3567214
Santiago Espindola <santiago2211ar@hotmail.com>
2024-01-26 09:14:41 -0300

    fix github button

--- src/utils/passport.config.js
+++ src/utils/passport.config.js
@@ -30,7 +30,7 @@ export function iniPassport() {
       {
         clientID: env.githubClientId,
         clientSecret: env.githubClientSecret,
-        callbackURL: "http://localhost:10000" + "/api/sessions/githubcallback",
+        callbackURL: env.apiUrl + "/api/sessions/githubcallback",
       },
       passportController.github
     )
